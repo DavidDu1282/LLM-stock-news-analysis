6fcb7b497613df6854d4f14c04269ebff1adb4c0
David Du <david2001.du@gmail.com>
2025-06-21 12:29:28 -0700

    Adding second email

--- config.py
+++ config.py
@@ -1,37 +1,29 @@
-# app/core/config.py
+# config.py
 import os
 
 from google import genai
+from pydantic import field_validator
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    # SECRET_KEY: str
-    # ALGORITHM: str = "HS256"
-    # ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    # REFRESH_TOKEN_EXPIRE_DAYS: int = 7
-
-    # DATABASE_URL: str
-
-    # POSTGRES_DB: str
-    # POSTGRES_USER: str
-    # POSTGRES_PASSWORD: str
-
-    # REDIS_HOST: str = "redis"
-    # REDIS_PORT: int = 6379
-    # REDIS_URL: str = f"redis://{REDIS_HOST}:{REDIS_PORT}"
-
-    # SERVER_IP: str = "0.0.0.0" 
-
     GOOGLE_PROJECT_ID: str
     GOOGLE_REGION: str
     GOOGLE_API_KEY: str
     GMAIL_USERNAME: str
     GMAIL_APP_PASSWORD: str
-    RECEIVER_EMAIL: str
+    RECEIVER_EMAIL: list[str]
     DEBUG: bool = False
     TUSHARE_API_TOKEN: str
     ALPHA_VANTAGE_API_KEY: str
     FINNHUB_API_KEY: str
+    
+    @field_validator("RECEIVER_EMAIL", mode="before")
+    @classmethod
+    def split_receiver_emails(cls, v: str) -> list[str]:
+        if isinstance(v, str):
+            # remove brackets and split by comma
+            return [email.strip() for email in v.strip("[]").split(",")]
+        return v
     
     class Config:
         env_file = ".env"
@@ -56,4 +48,4 @@ def load_gemini_api_key():
 
 GEMINI_API_KEY = load_gemini_api_key()
 gemini_client = genai.Client(api_key=GEMINI_API_KEY)
-# print(settings.model_dump())
+print(settings.model_dump())
